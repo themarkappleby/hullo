ba13c6e924eac4b0c4a5e237c6a2d07d9c80d835
Mark Appleby <themarkappleby@gmail.com>
2024-02-27 08:29:10 -0500

    Funtional peerjs

--- src/p2p.js
+++ src/p2p.js
@@ -5,29 +5,33 @@ const NAMESPACE = 'hullo'
 
 export const create = (id, stream) => {
     const peer = new Peer(`${NAMESPACE}-${id}`);
-    peer.on('connection', conn => {
-        console.log('connection')
-        conn.on('data', data => {
-            console.log(data);
+    peer.on('open', () => {
+        peer.on('connection', conn => {
+            conn.on('data', data => {
+                console.log(data);
+            });
         });
-    });
-    // peer.on('call', call => {
-    //     call.answer(stream)
-    //     call.on('stream', s => {
-    //     });
-    // });
+        peer.on('call', call => {
+            call.answer(stream)
+            call.on('stream', s => {
+                console.log(s);
+            });
+        });
+    })
     peer.on('error', error => console.error(error));
 }
 
 export const join = (id, stream) => {
     const peer = new Peer(`${NAMESPACE}-${getRandom(1000, 9999)}`);
-    const conn = peer.connect(`${NAMESPACE}-${id}`);
-    conn.on('open', () => {
-        console.log('open')
-        conn.send('hi!');
-    });
-    // const call = peer.call(`${NAMESPACE}-${id}`, stream)
-    // call.on('stream', stream => {
-    // });
+    peer.on('open', () => {
+        const conn = peer.connect(`${NAMESPACE}-${id}`);
+        conn.on('open', () => {
+            conn.send('hi!');
+        });
+        const call = peer.call(`${NAMESPACE}-${id}`, stream)
+        call.on('stream', s => {
+            console.log(s)
+        });
+    })
     peer.on('error', error => console.error(error));
 }
