51aa10402972e9abafead951f406d292be33473c
Mark Appleby <themarkappleby@gmail.com>
2024-04-14 13:29:15 -0400

    Switch to Metered TURN server

--- src/participant.js
+++ src/participant.js
@@ -53,7 +53,33 @@ class Participant {
 
     initPeer() {
         return new Promise((resolve) => {
-            this.peer = new Peer(this.id);
+            this.peer = new Peer(this.id, {
+                iceServers: [
+                    {
+                      urls: "stun:stun.relay.metered.ca:80",
+                    },
+                    {
+                      urls: "turn:global.relay.metered.ca:80",
+                      username: "cfd859b36d876badbf6f84c0",
+                      credential: "8+5bIpSvsOfgFFHD",
+                    },
+                    {
+                      urls: "turn:global.relay.metered.ca:80?transport=tcp",
+                      username: "cfd859b36d876badbf6f84c0",
+                      credential: "8+5bIpSvsOfgFFHD",
+                    },
+                    {
+                      urls: "turn:global.relay.metered.ca:443",
+                      username: "cfd859b36d876badbf6f84c0",
+                      credential: "8+5bIpSvsOfgFFHD",
+                    },
+                    {
+                      urls: "turns:global.relay.metered.ca:443?transport=tcp",
+                      username: "cfd859b36d876badbf6f84c0",
+                      credential: "8+5bIpSvsOfgFFHD",
+                    },
+                ],
+            });
             this.peer.on('error', error => {
                 console.error(error)
                 if (window.confirm('A connection error occurred. Please try again.')) {
