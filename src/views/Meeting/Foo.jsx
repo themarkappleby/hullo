73da7c1664bc0b380142a8eeb38b09d83df70fba
Mark Appleby <themarkappleby@gmail.com>
2024-04-19 09:23:03 -0400

    WIP rounded rect

---
+++ src/views/Meeting/Foo.jsx
@@ -0,0 +1,37 @@
+// Ref: https://discourse.threejs.org/t/roundedrectangle-squircle/28645
+function RectangleRounded( w, h, r, s ) { // width, height, radiusCorner, smoothness
+    
+    const pi2 = Math.PI * 2;
+    const n = ( s + 1 ) * 4; // number of segments    
+    let indices = [];
+    let positions = [];
+ 	let uvs = [];   
+    let qu, sgx, sgy, x, y;
+    
+	for ( let j = 1; j < n + 1; j ++ ) indices.push( 0, j, j + 1 ); // 0 is center
+    indices.push( 0, n, 1 );   
+    positions.push( 0, 0, 0 ); // rectangle center
+    uvs.push( 0.5, 0.5 );   
+    for ( let j = 0; j < n ; j ++ ) contour( j );
+    
+    const geometry = new THREE.BufferGeometry( );
+    geometry.setIndex( new THREE.BufferAttribute( new Uint32Array( indices ), 1 ) );
+	geometry.setAttribute( 'position', new THREE.BufferAttribute( new Float32Array( positions ), 3 ) );
+	geometry.setAttribute( 'uv', new THREE.BufferAttribute( new Float32Array( uvs ), 2 ) );
+    
+    return geometry;
+    
+    function contour( j ) {
+        
+        qu = Math.trunc( 4 * j / n ) + 1 ;      // quadrant  qu: 1..4         
+        sgx = ( qu === 1 || qu === 4 ? 1 : -1 ) // signum left/right
+        sgy =  qu < 3 ? 1 : -1;                 // signum  top / bottom
+        x = sgx * ( w / 2 - r ) + r * Math.cos( pi2 * ( j - qu + 1 ) / ( n - 4 ) ); // corner center + circle
+        y = sgy * ( h / 2 - r ) + r * Math.sin( pi2 * ( j - qu + 1 ) / ( n - 4 ) );   
+ 
+        positions.push( x, y, 0 );       
+        uvs.push( 0.5 + x / w, 0.5 + y / h );       
+        
+    }
+    
+}
--- src/views/Meeting/Stage.jsx
+++ src/views/Meeting/Stage.jsx
@@ -1,4 +1,8 @@
 import { Environment } from '@react-three/drei'
+import RoundedRect from './RoundedRect'
+
+const SIZE = 1;
+const RATIO = 3 / 4;
 
 export default gltf => {
   const ref = gltf.nodes.Plane
@@ -18,6 +22,14 @@ export default gltf => {
         shadow-camera-bottom={-30}
       />
       <Environment files="/images/rustig_koppie_puresky_1k.hdr" background />
+      <RoundedRect
+        position={[0, 1.15, -2]}
+        rotation={[0, 180 * (Math.PI / 180), 0]}
+        width={SIZE * RATIO}
+        height={SIZE}
+        depth={0.15}
+        radius={0.05}
+      />
       <group dispose={null}>
         <mesh castShadow receiveShadow geometry={ref.geometry} material={ref.material} position={[0, 0, 0]} />
       </group>
