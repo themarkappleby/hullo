47cee6bc197d97130bc1640976a319b2251c7787
Mark Appleby <themarkappleby@gmail.com>
2025-02-09 14:12:57 -0500

    Basic P2P mesh messaging UI

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ window.peer = new MeshPeer();
 document.getElementById('id').textContent = 'loading...';
 peer.on('open', () => {
     document.getElementById('id').textContent = peer.id;
-    peer.on('connection', initConnection);
+    peer.on('connection', handleConnection);
 });
 
 const joinMeetingForm = document.getElementById('joinMeetingForm');
@@ -12,12 +12,21 @@ joinMeetingForm.addEventListener('submit', e => {
     e.preventDefault();
     const meetingId = joinMeetingForm.meetingId.value;
     const connection = peer.connect(meetingId);
-    connection.on('open', () => initConnection(connection));
+    connection.on('open', () => handleConnection(connection));
 });
 
-function initConnection (connection) {
+const sendMessageForm = document.getElementById('sendMessageForm');
+sendMessageForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = sendMessageForm.message.value;
+    document.getElementById('messages').innerHTML += `<li>${message}</li>`;
+    e.target.reset();
+    peer.broadcast(message);
+});
+
+function handleConnection (connection) {
     document.getElementById('connections').innerHTML += `<li>${connection.peer}</li>` ;
     connection.on('data', (data) => {
-        console.log(data)
+        document.getElementById('messages').innerHTML += `<li>${data}</li>`;
     });
 }
