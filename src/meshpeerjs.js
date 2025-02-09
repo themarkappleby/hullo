4f547aa93cce0dadfd5abe246458e45d36e9fafb
Mark Appleby <themarkappleby@gmail.com>
2025-02-09 10:39:45 -0500

    Cleanup

--- src/meshpeerjs.js
+++ src/meshpeerjs.js
@@ -9,7 +9,6 @@ export default class MeshPeer extends Peer {
     }
 
     on(event, callback) {
-        console.log('event', event)
         if (event === 'connection') {
             super.on('connection', connection => {
                 const connectionsString = this.connections.map(c => c.peer).join(',')
@@ -27,7 +26,6 @@ export default class MeshPeer extends Peer {
     }
 
     connect(id) {
-        console.log('connecting to', id)
         if (this.connections.find(c => c.peer === id)) return;
         const self = this;
         const connection = super.connect(id);
@@ -35,9 +33,6 @@ export default class MeshPeer extends Peer {
             if (data.startsWith('meshpeerjs-connections:')) {
                 const connections = data.split(':').pop().split(',')
                 connections.forEach(id => {
-                    console.log('connecting to inner', id)
-                    // This isn't workring becaause a Peer instance can only have one connection at a time
-                    // I need to make a new Peer instance. MeshPeer should contain X Peer instances.
                     self.connect(id)
                 })
             }
@@ -46,34 +41,3 @@ export default class MeshPeer extends Peer {
         return connection;
     }
 }
-
-/*
-export const joinMeeting = meetingId => {
-    const promise = new Promise((resolve) => {
-        let peer = new Peer(meetingId);
-        peer.on('open', () => {
-            resolve(peer);
-        })
-        peer.on('error', error => {
-            if (error.type === 'unavailable-id') {
-                peer = new Peer();
-                peer.on('open', () => {
-                    resolve(peer);
-                    const connection = peer.connect(meetingId);
-                    connection.on('open', function(){
-                        connection.send(peer.id);
-                    });
-                })
-            } else {
-                console.error(error)
-            }
-        })
-        peer.on('connection', function(connection) {
-            connection.on('data', function(data){
-                console.log('recieved', data)
-            });
-        });
-    });
-    return promise;
-}
-*/
